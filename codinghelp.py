38bb5b707f3446b6e4aa1161fae2a6db4b35f472
Matt (IPv4) <matthew@cowley.org.uk>
2017-11-01 21:11:03 +0000

    make work with praw 5.2

--- codinghelp.py
+++ codinghelp.py
@@ -10,27 +10,16 @@ if not os.path.isfile("conf.py"):
     exit(1)
 
 # Create the Reddit instance
-user_agent = ("SupremeRedditB0t 0.3")
-r = praw.Reddit(user_agent=user_agent)
-
-# and login
-r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
-
-# Have we run this code before? If not, create an empty list
-if not os.path.isfile("codinghelp_posts_replied_to.txt"):
-    posts_replied_to = []
-
-# If we have run the code before, load the list of posts we have replied to
-else:
-    # Read the file into a list and remove any empty values
-    with open("codinghelp_posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(set(posts_replied_to))
+user_agent = ("SupremeRedditB0t 0.5")
+r = praw.Reddit(client_id=REDDIT_CLIENT,
+                client_secret=REDDIT_SECRET,
+                username=REDDIT_USERNAME,
+                password=REDDIT_PASS,
+                user_agent=user_agent)
 
 # Get all posts from subreddit
-subreddit = r.get_subreddit('CodingHelp')
-for submission in subreddit.get_new(limit=100):
+subreddit = r.subreddit('CodingHelp')
+for submission in subreddit.new(limit=25):
 
     # Check if has a flair, locked, archived, quarantined, stickied
     if submission.link_flair_text == None and submission.locked == False and submission.archived == False and submission.quarantine == False and submission.hidden == False and submission.stickied == False:
@@ -46,8 +35,14 @@ for submission in subreddit.get_new(limit=100):
         age_mins = (age.days * 24 * 60) + (age.seconds/60)
         age = age.days
 
+        # Check if we've replied
+        if submission.comments:
+            submission.comments.replace_more(limit=0)
+            replied = [f for f in submission.comments.list() if str(f.author) == str(r.user.me())]
+        else: replied = False
+
         # If we haven't replied to this post before and older than 10 mins
-        if submission.id not in posts_replied_to and age_mins >= 10:
+        if not replied and age_mins >= 10:
 
             # Add Instructions to footer
             footer = "\n\n&nbsp;\n\n*****\n\nTo add a flair:\n\n+ Click `flair` underneath your post\n\n+ Select a flair\n\n+ Click `save`" + footer
@@ -83,28 +78,20 @@ for submission in subreddit.get_new(limit=100):
                 flair = "Other Code"
             
             # Reply to the post with suggested flair
-            submission.add_comment("Please Add A Flair To Your Post!\n\nSuggested Flair: `["+flair+"]`" + footer)
-##            print("NEW Post:", submission.title.translate(non_bmp_map), "\n\tFlair:", flair, "\n\n")
-
-            # Store the current id into our list
-            posts_replied_to.append(submission.id)
-
-            # Write our updated list back to the file
-            with open("codinghelp_posts_replied_to.txt", "w") as f:
-                for post_id in posts_replied_to:
-                    f.write(post_id + "\n")
+            submission.reply("Please Add A Flair To Your Post!\n\nSuggested Flair: `["+flair+"]`" + footer)
+            #print("NEW Post:", submission.title.translate(non_bmp_map), "\n\tFlair:", flair, "\n\n")
 
         # If already replied and age >= 2, comment and delete
         elif age >= 2:
 
             # Output Alert
-##            print("REMOVE Post:", submission.title.translate(non_bmp_map), "\n\tHas an age of", age, "days with no flair so is being deleted\n\n")
+            #print("REMOVE Post:", submission.title.translate(non_bmp_map), "\n\tHas an age of", age, "days with no flair so is being deleted\n\n")
             
             # Reply to the post with deletion message
-            submission.add_comment("Due to your post being submitted for two days or more without a flair, it has been deleted.\n\nWhen posting in future, make sure to add a flair once the post is submitted." + footer)
+            submission.reply("Due to your post being submitted for two days or more without a flair, it has been deleted.\n\nWhen posting in future, make sure to add a flair once the post is submitted." + footer)
 
             # Flair Post
-            submission.set_flair("[Removed]")
+            submission.mod.flair(text='[Removed]')
 
             # Lock post
             submission.lock()
@@ -112,8 +99,8 @@ for submission in subreddit.get_new(limit=100):
             # Remove post
             submission.remove()
             
-##        elif age_mins >= 10:
-##            print("OLD Post:", submission.title.translate(non_bmp_map), "\n\tHas an age of", age, "days with no flair\n\n")
+        #elif age_mins >= 10:
+            #print("OLD Post:", submission.title.translate(non_bmp_map), "\n\tHas an age of", age, "days with no flair\n\n")
 
 # Alert Completion
 print("r/CodingHelp Bot Scan Completed")
