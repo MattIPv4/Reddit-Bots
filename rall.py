a0eba20b4ea8f6a8bc4bf21f710e957318bdfc59
Matt (IPv4) <matthew@cowley.org.uk>
2017-11-01 21:12:28 +0000

    praw 5.2 | no file | mostly works

--- rall.py
+++ rall.py
@@ -6,51 +6,38 @@ non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 # Check that the file that contains our username exists
 if not os.path.isfile("conf.py"):
-##    print("Config Required!")
     exit(1)
 
 # Create the Reddit instance
-user_agent = ("SupremeRedditB0t 0.3")
-r = praw.Reddit(user_agent=user_agent)
-
-# and login
-r.login(REDDIT_USERNAME, REDDIT_PASS, disable_warning=True)
-
-# Have we run this code before? If not, create an empty list
-if not os.path.isfile("rall_posts_replied_to.txt"):
-    posts_replied_to = []
-
-# If we have run the code before, load the list of posts we have replied to
-else:
-    # Read the file into a list and remove any empty values
-    with open("rall_posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(set(posts_replied_to))
+user_agent = ("SupremeRedditB0t 0.5")
+r = praw.Reddit(client_id=REDDIT_CLIENT,
+                client_secret=REDDIT_SECRET,
+                username=REDDIT_USERNAME,
+                password=REDDIT_PASS,
+                user_agent=user_agent)
 
 # Define blocked subs
-blockedSubs = [
-    'TodayILearned'
-]
+blockedSubs = ['TodayILearned']
 
 # Define blocked users
-blockedUsers = [
-    'mike_pants',       '_5__',
-    'exoticmind_2',     'misterdominic',
-    'Yung_Relight',     'LogansGamerta9',
-    'RicoDePico',       'Tjah78',
-    'createdin2017',    'Pmaguire13'
-]
+blockedUsers = ['mike_pants', '_5__', 'exoticmind_2', 'misterdominic', 'Yung_Relight', '​LogansGamerta9', '​RicoDePico', '​Tjah78', 'c​reatedin2017', 'Pmaguire13']
 
 # Get all posts from subreddit
-subreddit = r.get_subreddit('all')
+subreddit = r.subreddit('all')
 count = 0
 errors = 0
-for submission in subreddit.get_top_from_day(limit=50):
+for submission in subreddit.top("day", limit=25):
 
-    # Check if is locked, archived, quarantined, hidden, stickied, in blacklist, in already replied
-    if submission.locked == False and submission.archived == False and submission.quarantine == False and submission.hidden == False and submission.stickied == False:
-        if str(submission.id) not in posts_replied_to and str(submission.subreddit) not in blockedSubs and str(submission.author) not in blockedUsers:
+    # Check if is locked, archived, quarantined, hidden, stickied, in already replied
+    if submission.subreddit.user_is_banned == False and submission.locked == False and submission.archived == False and submission.quarantine == False and submission.hidden == False and submission.stickied == False:
+
+        # Check if we've replied (upvoted)
+        if submission.likes:
+            replied = submission.likes
+        else: replied = False
+
+        # If we haven't replied to this post before and not blacklisted user/sub
+        if not replied and str(submission.subreddit) not in blockedSubs and str(submission.author) not in blockedUsers:
 
             # Generic Bot Footer (Using []() to avoid mention, replacing ' ' with ' ^' to superscript everything)
             footer = "\n&nbsp;\n\n*****\n\n*^I am a bot, probably quite annoying, I mean no harm though*"
@@ -59,23 +46,21 @@ for submission in subreddit.get_top_from_day(limit=50):
 
             # Reply to the post
             try:
-                submission.add_comment("Congrats for reaching r/all/top/ (of the day, top 50) with your post!" + footer)
-                submission.upvote()
-            except:
+                submission.reply("Congrats for reaching r/all/top/ (of the day, top 25) with your post!" + footer)
+            except Exception as e:
                 errors += 1
-    ##            print("Comment Failed...\n")
-    ##            print("Unexpected error:", sys.exc_info()[0], "\n\n")
+                #print("Comment Failed...\n")
+                #print("Unexpected error:", e, "\n\n")
             else:
-                count += 1
-    ##            print("NEW Post ["+str(count)+"]:", submission.title.translate(non_bmp_map), "\n\n")
-
-            # Store the current id into our list
-            posts_replied_to.append(submission.id)
-
-            # Write our updated list back to the file
-            with open("rall_posts_replied_to.txt", "w") as f:
-                for post_id in posts_replied_to:
-                    f.write(post_id + "\n")
+                try:
+                    submission.upvote()
+                except Exception as e:
+                    errors += 1
+                    #print("Upvote Failed...\n")
+                    #print("Unexpected error:", e, "\n\n")
+                else:
+                    count += 1
+                    #print("NEW Post ["+str(count)+"]:", submission.title.translate(non_bmp_map), "\n\n")
 
 # Alert Completion
 print("r/all/top/ Bot Scan Completed")
